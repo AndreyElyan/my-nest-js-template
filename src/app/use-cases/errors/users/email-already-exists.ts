ec86bb37e2af54fa95219443cc707bdd452e43d5
Andrey Elyan <andrey.silveira@datum.inf.br>
2022-12-21 10:15:38 -0300

    feat(swagger): integrated swagger to api :memo:

--- src/app/use-cases/errors/users/email-already-exists.ts
+++ src/app/use-cases/errors/users/email-already-exists.ts
@@ -2,6 +2,6 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class EmailAlreadyExistsError extends HttpException {
   constructor() {
-    super('Email Already Exists', HttpStatus.FORBIDDEN);
+    super('Email Already Exists', HttpStatus.BAD_REQUEST);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { HttpExceptionFilter } from '@app/exceptions/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Long Trip Backend API')
+    .setDescription('Backend API for Long Trip')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
